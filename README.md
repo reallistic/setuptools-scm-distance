e16390e8b4cdfacaf3316b2a27b419a535f62663
Michael Chase <3686226+reallistic@users.noreply.github.com>
2026-02-05 12:09:09 -0500

    Setup initial tag plus distance plugin

---
+++ README.md
@@ -0,0 +1,3 @@
+# setuptools-scm-distance
+
+setuptools-scm-distance is a Python 3.10+ setuptools-scm plugin that produces dynamic versions for pyproject.toml packages based on [setupmeta's distance strategy.](https://github.com/codrsquad/setupmeta/blob/main/docs/versioning.rst#distance)
---
+++ setuptools_scm_distance.py
@@ -0,0 +1,12 @@
+from setuptools_scm.version import SEMVER_LEN, ScmVersion
+
+
+def get_tag_distance(scm_version: ScmVersion) -> str:
+    if scm_version.exact:
+        return scm_version.format_with("tag")
+    else:
+        version_parts = list(scm_version.tag.release)
+        if len(version_parts) != SEMVER_LEN or version_parts[-1] != 0:
+            raise ValueError("tag must be in format v#.#.0")
+        version_parts[2] = scm_version.distance
+        return ".".join([str(a) for a in version_parts])

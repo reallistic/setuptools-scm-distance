a570fe414fce2e7a7dfc19d8c064baf268258c02
Michael Chase <3686226+reallistic@users.noreply.github.com>
2026-02-05 15:15:16 -0500

    better error for bad tag

--- setuptools_scm_distance.py
+++ setuptools_scm_distance.py
@@ -7,6 +7,6 @@ def get_tag_distance(scm_version: ScmVersion) -> str:
     else:
         version_parts = list(scm_version.tag.release)
         if len(version_parts) != SEMVER_LEN or version_parts[-1] != 0:
-            raise ValueError("tag must be in format v#.#.0")
+            raise ValueError(f"tag {scm_version.tag.release!r} is not in format v#.#.0")
         version_parts[2] = scm_version.distance
         return ".".join([str(a) for a in version_parts])
